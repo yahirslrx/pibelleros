0e5c1aec4b80aeb55c9b34c8e6f7fdde637cdf3c
yahirslrx <105768781+yahirslrx@users.noreply.github.com>
2025-03-03 18:43:29 -0700

    Delete chetonios directory

--- chetonios/prender.cmd
+++
@@ -1,7 +0,0 @@
-@echo off
-echo Iniciando el servidor...
-cd /d "C:\Users\yahir\Documents\chetonios"
-start node server.js
-timeout /t 2 >nul
-start http://localhost:3000
-echo Servidor iniciado. Abriendo la página...
